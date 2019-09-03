2abd55d4ff5df050d9458f7161e0b9c0cd690bd0
JDZC <48192417+JDZC@users.noreply.github.com>
2019-09-03 19:47:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-"""importa o reconhecimento de voz"""
+"""importa o reconhecimento de voz e o sistema operacional"""
 
 import speech_recognition as sr 
 
+"""reconhece a variável R como o reconhecimento de voz"""
 r = sr.Recognizer() 
 
-""" define a variável para reconhecimento de voz"""
-
-with sr.Microphone() as source 
-"""identifica o mic como fonte""" 
-	r.adjust_for_ambient_noise(source) 
-	""" Ajusta o mic para reconhecer a voz ambiente"""
-	while True: 
-		audio = r.listen(source)
-		"""escuta da fonte source"""
-		speech = r.recognize_google(audio, language='pt' )
-		
-		print('Você disse isso aqui o seu coco: ', speech)
-		"""imprime o que o usuário disse"""	
-		
+"""prepara o microfone como fonte do som e printa fale algo, depois pede para a variável áudio ser escutada pelo mic"""
+with sr.Microphone() as source: 
+	print('Fala algo: ') 
+	audio = r.listen(source)
+"""tenta reconhecer o texto através da API da google na linguagem em português"""
+	try:
+		text = r.recognize_google(audio, language='pt')
+		print("Tu disse: {}".format(text))
+"""caso não entenda, imprime "Numtendi"""
+	except:
+		print('num tendi')
