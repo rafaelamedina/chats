844c5d52eb55f2a0d8e70fe0cd403df70c27a8d0
JDZC <48192417+JDZC@users.noreply.github.com>
2019-09-03 20:37:18 -0300

    Update mainsmn.py

--- mainsmn.py
+++ mainsmn.py
@@ -3,24 +3,37 @@ from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 import os 
 import speech_recognition as sr 
+
 """define o chatbot como smn (senhor meia noite)"""
 bot = ChatBot("smn")
 
 """Treinador do bot, está comentado pois sua utilidade é apenas o treinamento de novas palavras para o bot. No exemplo de conversação, é um início de diálogo"""
 """conversation = [
     "Olá",
-    "E aí!",
-    "Como você vai?",
-    "Estou indo bem",
-    "Legal",
-    "Valeu",
-    "Que bom!"
+    "Olá",
+    "Oii",
+    
+]"""
+
+"""conversation = [
+    "Tudo bem?",
+    "Ótimo! e você?",
+    "Estou bem e você?",
+]
+conversation = [
+    "Também?",
+    "Bom ouvir isso!",
+    "Que bom!",
+    "Adoro ver pessoas felizes",
 ]
 
-trainer = ListTrainer(chatbot)
 
-trainer.train(conversation)"""
 
+
+trainer = ListTrainer(bot)
+trainer.train(conversation)
+
+"""
 """while True:
 message = input('Tu: ')
 if message.strip() != 'Tchau Tchau':
@@ -29,11 +42,14 @@ print('ChatBot:',reply)
 if message.strip() == 'Tchau Tchau':
 print('ChatBot : Tchau!')
 break""" """teste de chat, ainda não funcional"""
+
 while True:
     try:
+        
         bot_input = bot.get_response(input())
-        print('Bob: ', bot_input)
+        print('Bob:', bot_input)
 
     except(KeyboardInterrupt, EOFError, SystemExit):
         break
-		"""faz o chat funcionar"""
+		
+       
